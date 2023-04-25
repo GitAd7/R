6b50b6ca90357f7bc84114472ce249684e6f8280
Adesh Singh Tomar <107893872+GitAd7@users.noreply.github.com>
2023-04-26 00:19:09 +0530

    Add files via upload
    Learnt basic concepts of R

---
+++ Basic R concepts.R
@@ -0,0 +1,64 @@
+## Variables in R Programming
+var_name <- 10
+var.name <- 10
+_var_name <-10
+var_name* <-10
+.var_name <-10
+5var_name <-10
+## Assignment of Variable
+var1 <- 10
+var2 = 10
+10 -> var3
+print(var1)
+cat(var1,"",var2,"\n",var3)
+
+## Data Type 
+num<- 10
+class(num)
+typeof(num)
+
+int <- 15L
+class(int)
+int1 <- 15
+class(int1)
+int1 <- as.integer(int1)
+class(int1)
+
+## Conversion of data types in R
+num1 <- as.numeric(24L)
+class(num1)
+# we can use as. to convert any data types into one another
+
+## Operators in R
+a <- 7.5
+b <- 2
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%%b)
+print(a%/%b)
+print(a^b)
+
+c1 <- c(8,9,10)
+c2 <- c(4,5,6)
+print(c1+c2)
+print(c1-c2)
+print(c1*c2)
+print(c1/c2)
+print(c1%%c2)
+print(c1+%/%c2)
+print(c1^c2)
+
+a <- 7.5
+b <- 2
+print(a>b)
+print(a<b)
+print(a=b)
+print(a<=b)
+print(a>=b)
+print(a!=b)
+
+d <- c(3.5, TRUE, 2+5i)
+e <- c(0, TRUE, 6-5i)
+print(d&&e)
